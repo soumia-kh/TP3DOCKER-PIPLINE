e20b8f86a9361b4b8b87af372eb29421225867a8
soumia-kh <sou.elkhayari@edu.umi.ac.ma>
2026-03-17 01:30:22 +0000

    add

--- tests/test_tasks.py
+++ tests/test_tasks.py
@@ -1,12 +1,20 @@
 import sys
 import os
+
 sys.path.append(os.path.abspath(os.path.join(os.path.dirname(__file__), "../app")))
+
+from tasks import add_task, get_tasks, tasks
+
+def setup_function():
+    tasks.clear()
+
 def test_add_task():
     add_task("learn Docker")
     assert "learn Docker" in get_tasks()
+
 def test_multiple_tasks():
     add_task("Learn CI")
     add_task("Learn DevOps")
-    tasks = get_tasks()
-    assert "Learn CI" in tasks
-    assert "Learn DevOps" in tasks    
+    current_tasks = get_tasks()
+    assert "Learn CI" in current_tasks
+    assert "Learn DevOps" in current_tasks
