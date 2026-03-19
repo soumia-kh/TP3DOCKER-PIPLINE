fb784362786f0e06176675892c13cc01ebfb1acf
soumia-kh <sou.elkhayari@edu.umi.ac.ma>
2026-03-19 14:10:06 +0000

    test ci

--- tests/test_tasks.py
+++ tests/test_tasks.py
@@ -1,20 +1,17 @@
 import sys
 import os
-
+# On ajoute le dossier 'app' au path pour pouvoir importer tasks
 sys.path.append(os.path.abspath(os.path.join(os.path.dirname(__file__), "../app")))
 
-from tasks import add_task, get_tasks, tasks
-
-def setup_function():
-    tasks.clear()
+from tasks import add_task, get_tasks
 
 def test_add_task():
-    add_task("learn Docker")
+    res = add_task("learn Docker")
     assert "learn Docker" in get_tasks()
 
 def test_multiple_tasks():
     add_task("Learn CI")
     add_task("Learn DevOps")
-    current_tasks = get_tasks()
-    assert "Learn CI" in current_tasks
-    assert "Learn DevOps" in current_tasks
+    tasks = get_tasks()
+    assert "Learn CI" in tasks
+    assert "Learn DevOps" in tasks
